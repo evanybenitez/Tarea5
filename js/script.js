4ecac620d97597ffa143c7feb149533a9cdc246d
Evany <evanygaleano@hotmail.com>
2016-09-06 20:58:17 +0200

    tarea5 Evany Benitez Galeano

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// es un comentario script.js
+/* Comentario 
+multi
+linea
+*/
+//FUNCION ANONIMA
+	(function(){
+		console.log('Hola Mundo');
+	})();
+
+	// una variable de tipo numerico
+	var num = 123;
+	// una variable de tipo string
+	var texto = 'Hola Mundo JS';
+	//una variable de tipo array
+	var lista = [];
+	//una variable de tipo object
+	var persona = {};
+
+	// 2 elementos del tipo number del array
+	lista.push(12);
+	lista.push(76);
+
+	console.log(lista);
+
+	var miobjeto ={};//variable tipo object
+
+	//los atributos descripcion y cantidad a la variable del tipo object
+
+	miobjeto.descripcion = "celular";
+	miobjeto.cantidad = 23;
+
+	console.log(miobjeto);
